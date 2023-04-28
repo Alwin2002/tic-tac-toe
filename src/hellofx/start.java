54999d1cb018838ab644414096d5fb2ae8f36829
alwin <alwinantu0909@gmail.com>
2023-04-28 21:16:52 +0530

    commit it

--- src/hellofx/start.java
+++ src/hellofx/start.java
@@ -1,7 +1,5 @@
 package hellofx;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -11,9 +9,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
-
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -21,18 +18,20 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class start extends Application{
     VBox vbox = new VBox();
 
     
-    
+    @Override
     public void start(Stage primaryStage) throws ClassNotFoundException, SQLException, IOException{
 
         Random rand = new Random();
@@ -44,7 +43,7 @@ public class start extends Application{
         database db = new database();
         TextField text1 = new TextField();
         text1.setPromptText("Enter Username");
-        TextField text2 = new TextField();
+        PasswordField text2 = new PasswordField();
         text2.setPromptText("Enter password");
         Button b1 = new Button("Login In");
         Button b2 = new Button("Sign Up");
@@ -64,15 +63,17 @@ public class start extends Application{
         TextField tx2 = new TextField();
         VBox vbox2 = new VBox();
         Button b3 = new Button("Sign Up");
+        Button b6 = new Button("Back");
         tx1.setPromptText("Enter Username");
         tx2.setPromptText("Enter Password");
 
-        vbox2.getChildren().addAll(tx1,tx2,b3);
+        vbox2.getChildren().addAll(tx1,tx2,b3,b6);
         vbox2.setAlignment(Pos.CENTER);
         vbox2.setSpacing(20);
         vbox2.setPadding(new Insets(30));
         Scene scene2 = new Scene(vbox2,300,300);
 
+        PauseTransition delay = new PauseTransition(Duration.seconds(3));
 
         
         
@@ -105,7 +106,7 @@ public class start extends Application{
             str[i]= rst.getString(1);
             tex[i]= new Text(rst.getString(1));
             hbox2[i].getChildren().add(tex[i]);
-            hbox2[i].setPrefWidth(200);
+            hbox2[i].setPrefWidth(340);
             hbox[i].getChildren().addAll(hbox2[i],butt[i]);
             items.add(hbox[i]);
             rst.next();
@@ -113,9 +114,12 @@ public class start extends Application{
         list.setItems(items);
         list.setStyle("-fx-border-color: white;");
         Button b5 = new Button("Accept Invitation");
+
         VBox vbox4 = new VBox();
         vbox4.getChildren().addAll(b5,list);
-        Scene scene3 = new Scene(vbox4,300,300);
+        vbox4.setAlignment(Pos.CENTER);
+        VBox.setMargin(b5, new Insets(10));
+        Scene scene3 = new Scene(vbox4,400,400);
 
 
         //Creating Scene 4 
@@ -174,39 +178,50 @@ public class start extends Application{
             String s3 = InetAddress.getLocalHost().getHostAddress().toString();
             String query = "INSERT INTO tic_tac_toe (Username, Password, IP)VALUES ('"+s1+"','"+s2+"','"+s3+"');";
             stmt.executeUpdate(query);
+            primaryStage.setScene(scene1);
         } catch (SQLException | UnknownHostException e1) {
             e1.printStackTrace();
         } 
     } 
         );  
 
+        b6.setOnAction(e-> primaryStage.setScene(scene1));
+
 
         b5.setOnAction(e -> {
             primaryStage.setScene(scene5);
-            try {
-                Socket socket2 = ss.accept();
-                Main main = new Main();
-                primaryStage.setScene(main.get(socket2)); 
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+                delay.play();
+                delay.setOnFinished(event -> { 
+                    try (Socket socket2 = ss.accept()) {
+                        Main main = new Main();
+                        primaryStage.setScene(main.get(socket2));
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    } 
+                });
+           
         });
 
         
         for(int i=0;i<size;i++){
             final int ii = i;
             butt[i].setOnAction(e-> {
-                String queryString = "select port from tic_tac_toe where Username ='"+str[ii]+"'";
-                System.out.println(queryString);
-                try (ResultSet rst2 = stmt.executeQuery(queryString)) {
-                    rst2.next();
-                    primaryStage.setScene(scene4);
-                    Socket socket1 = new Socket("127.0.0.1",rst2.getInt(1));
-                    client cli = new client();
-                    primaryStage.setScene(cli.get(socket1)); 
-                } catch (SQLException | IOException e1) {
-                    e1.printStackTrace();
-                }
+                primaryStage.setScene(scene4);
+                delay.play();
+                delay.setOnFinished(event -> { 
+                    String queryString = "select port from tic_tac_toe where Username ='"+str[ii]+"'";
+                    System.out.println(queryString);
+                    try (ResultSet rst2 = stmt.executeQuery(queryString)) {
+                        rst2.next();
+                        
+                        Socket socket1 = new Socket("127.0.0.1",rst2.getInt(1));
+                        client cli = new client();
+                        primaryStage.setScene(cli.get(socket1)); 
+                    } catch (SQLException | IOException e1) {
+                        e1.printStackTrace();
+                    }
+                });
+               
                 });
         }
 
