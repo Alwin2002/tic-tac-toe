4580d2b5b9cb8b2ad5d8146f6c83ab7ee9306ce3
alwin <alwinantu0909@gmail.com>
2023-04-28 11:55:20 +0530

    working with database

--- src/hellofx/signup.java
+++ src/hellofx/signup.java
@@ -1,10 +1,13 @@
 package hellofx;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.sql.SQLException;
@@ -13,16 +16,21 @@ import java.sql.Statement;
 public class signup {
     TextField t1 = new TextField();
     TextField t2 = new TextField();
-    TextField t3 = new TextField();
     VBox vbox = new VBox();
     Button b1 = new Button("Sign Up");
     database db = new database();
+    Stage p;
+
+    signup(Stage p){
+        this.p=p;
+    }
 
     public Scene get() throws ClassNotFoundException, SQLException, UnknownHostException{
         t1.setPromptText("Enter Username");
         t2.setPromptText("Enter Password");
-        t3.setPromptText("Confirm Password");
-        vbox.getChildren().addAll(t1,t2,t3,b1);
+
+        vbox.getChildren().addAll(t1,t2,b1);
+        vbox.setAlignment(Pos.CENTER);
         vbox.setSpacing(20);
         vbox.setPadding(new Insets(30));
         Scene s = new Scene(vbox,300,300);
@@ -30,18 +38,24 @@ public class signup {
         Statement stmt = db.get();
 
 
-        b1.setOnAction(e -> {try {
+        b1.setOnAction(e -> {
+            try {
             String s1 = t1.getText();
             String s2 = t2.getText();
             String s3 = InetAddress.getLocalHost().getHostAddress().toString();
             String query = "INSERT INTO tic_tac_toe (Username, Password, IP)VALUES ('"+s1+"','"+s2+"','"+s3+"');";
-            System.out.print(query);
             stmt.executeUpdate(query);
         } catch (SQLException e1) {
             e1.printStackTrace();
         } catch (UnknownHostException e1) {
             e1.printStackTrace();
-        }});
+        }
+
+    }
+        
+        
+        
+        );
         return s;
     }
 }
--- src/hellofx/start.java
+++ src/hellofx/start.java
@@ -1,14 +1,20 @@
 package hellofx;
 
+import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -18,10 +24,10 @@ public class start extends Application{
 
     
     
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) throws ClassNotFoundException, SQLException{
 
 
-
+        database db = new database();
         TextField text1 = new TextField();
         text1.setPromptText("Enter Username");
         TextField text2 = new TextField();
@@ -29,24 +35,113 @@ public class start extends Application{
         Button b1 = new Button("Login In");
         Button b2 = new Button("Sign Up");
         Text t = new Text("OR");
-        vbox.getChildren().addAll(text1,text2,b1,t,b2);
+        Text t2 = new Text();
+        vbox.getChildren().addAll(text1,text2,t2,b1,t,b2);
         vbox.setAlignment(Pos.CENTER);
 
 
-        b2.setOnAction(e -> {try {
-            primaryStage.setScene(new signup().get());
-        } catch (ClassNotFoundException | UnknownHostException | SQLException e1) {
-            e1.printStackTrace();
-        }});
-
-
+        
         vbox.setSpacing(20);
         vbox.setPadding(new Insets(30));
         Scene scene1 = new Scene(vbox,300,350);
 
+
+        TextField tx1 = new TextField();
+        TextField tx2 = new TextField();
+        VBox vbox2 = new VBox();
+        Button b3 = new Button("Sign Up");
+        tx1.setPromptText("Enter Username");
+        tx2.setPromptText("Enter Password");
+
+        vbox2.getChildren().addAll(tx1,tx2,b3);
+        vbox2.setAlignment(Pos.CENTER);
+        vbox2.setSpacing(20);
+        vbox2.setPadding(new Insets(30));
+        Scene scene2 = new Scene(vbox2,300,300);
+
+
+        ListView<HBox> list = new ListView<HBox>();
+        ObservableList<HBox> items =FXCollections.observableArrayList ();
+        
+           
+        Statement stmt = db.get();
+        String s4 = "select Username from tic_tac_toe;";
+        ResultSet rst =  stmt.executeQuery(s4);
+        rst.last();
+        int size = rst.getRow();
+        rst.first();
+
+
+        HBox[] hbox = new HBox[size];
+        HBox[] hbox2 = new HBox[size];
+        Button[] butt = new Button[size];
+        Text[] tex = new Text[size];
+
+        for(int i=0;i<size;i++){
+            hbox[i]= new HBox();
+            hbox2[i]= new HBox();
+            butt[i] = new Button("Play");
+            tex[i]= new Text(rst.getString(1));
+            hbox2[i].getChildren().add(tex[i]);
+            hbox2[i].setPrefWidth(200);
+            hbox[i].getChildren().addAll(hbox2[i],butt[i]);
+            items.add(hbox[i]);
+            rst.next();
+        }
+        list.setItems(items);
+        Scene scene3 = new Scene(list,300,300);
+
+
+
+
+
         primaryStage.setScene(scene1);
         primaryStage.setTitle("TIC-TAC-TOE");
         primaryStage.show();
+
+        b2.setOnAction(e -> primaryStage.setScene(scene2));
+
+        b1.setOnAction(e -> {
+           
+                try {
+
+                    String s1 = text1.getText();
+                    String s2 = text2.getText();
+                    String s3 = "select IP from tic_tac_toe where Username='"+s1+"' and Password='"+s2+"';";
+                    ResultSet rs = stmt.executeQuery(s3);
+                    System.out.println(s3);
+                    try {
+                        rs.next();
+                    System.out.println(rs.getString(1));
+                    primaryStage.setScene(scene3);
+                    }
+                    catch(Exception e2){
+                        t2.setText("Login Failed");
+                    }
+
+                } catch (SQLException e1) {
+
+                    e1.printStackTrace();
+                }
+            
+        });
+
+        b3.setOnAction(e -> {
+            try {
+
+            String s1 = tx1.getText();
+            String s2 = tx2.getText();
+            String s3 = InetAddress.getLocalHost().getHostAddress().toString();
+            String query = "INSERT INTO tic_tac_toe (Username, Password, IP)VALUES ('"+s1+"','"+s2+"','"+s3+"');";
+            stmt.executeUpdate(query);
+        } catch (SQLException | UnknownHostException e1) {
+            e1.printStackTrace();
+        } 
+    } 
+        );  
+
+
+
     }
     public static void main(String[] args) {
         launch(args);
